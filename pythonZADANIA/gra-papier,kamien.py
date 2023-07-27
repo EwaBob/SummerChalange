01b2fe26e0fdc35dac8e346d63892d9494f1bed9
EwaBob <114256392+EwaBob@users.noreply.github.com>
2023-07-27 13:05:44 +0200

    Add files via upload

---
+++ pythonZADANIA/gra-papier,kamien.py
@@ -0,0 +1,42 @@
+# Napisz grę kamień-papier-nożyce tak, aby korzystać z funkcji.
+
+
+import random
+
+def get_user_choice():
+    return input('Wybierz kamień, papier lub nożyce: ')
+
+def get_computer_choice():
+    choices = ['kamień', 'papier', 'nożyce']
+    return random.choice(choices)
+
+def play(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "REMIS! Zagraj jeszcze raz, żeby wygrać"
+
+    elif (user_choice == 'kamień' and computer_choice == 'nożyce') or \
+         (user_choice == 'papier' and computer_choice == 'kamień') or \
+         (user_choice == 'nożyce' and computer_choice == 'papier'):
+        return 'Wygrywasz!'
+    else:
+        return 'Przegrywasz!'
+
+def play_again():
+    play_again = input('Czy chcesz zagrać jeszcze raz? (tak/nie):' )
+    if play_again.lower() == 'tak':
+        return True
+    else:
+        return False
+
+def main():
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        result = play(user_choice, computer_choice)
+        print(f'Komputer wybrał: {computer_choice}')
+        print(result)
+        if not play_again():
+            break
+
+if __name__ == "__main__":
+    main()
